f24320b2839237e459784ba5dcab134e91dbe8ef
Rylie Breslin <33608974+BreslinRy@users.noreply.github.com>
2021-03-11 16:51:52 -0700

    Update MessageSender.cpp

--- Main/MessageSender.cpp
+++ Main/MessageSender.cpp
@@ -15,7 +15,7 @@ enum events{
   lowBattery, //easy, use code form the test example
   fullSDCard, //check to see if this is possible + how to do  => seems possible using a header file => look here https://forum.arduino.cc/index.php?topic=139852.0 => if not doing sd card detect why do this
   noSDCard, // if port is empty? look into => I think its possible, should be a pin on the breakoutboard that I can use to do this
-  noThermister //check to see how to do this => initial thought is seeing what happens when the thermister is disconeected and look for that => look for null on the port
+  noThermister, //check to see how to do this => initial thought is seeing what happens when the thermister is disconeected and look for that => look for null on the port
   periodicReport,
   powerRestored,
   deviceReset,
