017725f4dbbff6526c58fb832d37badc8e712a19
Rylie Breslin <33608974+BreslinRy@users.noreply.github.com>
2021-03-11 16:47:44 -0700

    Update MessageSender.hpp

--- Main/inc/MessageSender.hpp
+++ Main/inc/MessageSender.hpp
@@ -2,15 +2,24 @@
 #define _MESSAGE_SENDER_HPP_
 
 
-extern char errorStatus[32];
+
 extern char *messageText;
-extern char sendTo[8][16]; // How to send to two numbers => Loop that uses a different char every time?
+//extern char sendTo[8][16]; // How to send to two numbers => Loop that uses a different char every time?
+extern long timestamp;
+extern long errorDelay;
+extern static char* [10];
+
+extern enum events;
 
 
 void sendText();
 
 void chooseMessage();
 
-void operation();
+void blinkLED();
+
+void blinkShort();
+
+void blinkLong();
 
 #endif // _MESSAGE_SENDER_HPP_
