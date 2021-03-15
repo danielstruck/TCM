59e7a9e81c763a60b9c5bd60ff9eac98ba436b0b
Rylie Breslin <33608974+BreslinRy@users.noreply.github.com>
2021-03-15 10:08:30 -0700

    Update MessageSender.cpp

--- Main/MessageSender.cpp
+++ Main/MessageSender.cpp
@@ -65,34 +65,3 @@ void chooseMessage(int eventNum) {
       break;
   }
 }
-
-void blinkLED(int eventNum) {
-	// Error LED Sets
- 
-  for (int i = 0; i < 3; i++){
-    int bit = (eventNum >> i) & 1;
-
-    if (bit == 0)
-      blinkShort();
-    else 
-      blinkLong();
- 
- }
-
- delay(5000);
- 
-}
-
-void blinkShort(){
-  setErrorLEDOn();
-  delay(500);
-  setErrorLEDOff();
-  delay(500);
-}
-
-void blinkLong() {
-  setErrorLEDOn();
-  delay(1000);
-  setErrorLEDOff();
-  delay(500);
-}
