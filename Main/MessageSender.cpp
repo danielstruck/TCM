f4c2d07a8a950aeafdc78248e10c03f941daea8d
Rylie Breslin <33608974+BreslinRy@users.noreply.github.com>
2021-03-24 15:56:09 -0700

    Update MessageSender.cpp

--- Main/MessageSender.cpp
+++ Main/MessageSender.cpp
@@ -3,50 +3,54 @@
 #include "inc/HWIO.hpp"
 #include "inc/Errors.h"
 
+#define time_Max (4,294,967,295); //Max value of millis()
+#define fifteenMinutes (900,000); //Fifteen minutes worth of milliseconds
+
 char messageText[128];
 unsigned long timestamp;
 unsigned long errorDelay;
+int lastSentArray[10];
+int nextSentArray[10];
 
 
 //char sendTo[8][16] = {"7202449051", "2246160041"}; // How to send to two numbers => Loop that uses a different char every time?
 
 // make message for when profile is changed
 const char *messages[] = {
-  "Temperature out of range: (rangeMin - rangeMax) Current Temp: %d",
-  "Power outage. Current Battery %%: %d",
+  "WARNING: Temperature out of range: (rangeMin - rangeMax) Current Temp: %d",
+  "WARNING: Power outage. Current Battery %%: %d",
   "",
-  "Battery Low. Current Battery %%: %d",
+  "",//"WARNING: Battery Low. Current Battery %%: %d",
   "",
   "", 
-  "Thermister not detected.",
+  "WARNING: Thermister not detected.",
   "Periodic Report: Range is %d to %d. Current Temperature is %d",
   "POWER RESTORED: Current Battery %% is %d", 
   "Device reset button pressed. Restarting operations",
   ""
 };
 
-void sendText() {
+void sendText(int eventNum) {
   Serial.println("Send text function");
    //char *sendTo = "";
-	 // send an SMS!
-   static int counter = 0;
-   counter++;
-   if (counter ==  1) {
+	 // send an SMS!   
+   
+   chooseMessage(eventNum);
+   millisOverflow(eventNum);
+
+   unsigned long ms = millis();
+   
+   if (ms >= nextSentArray[eventNum]) {
       fona.sendSMS("7202449051", messageText); //send to => phone numbers
      // fona.sendSMS("2246160041", messageText); //send to => phone numbers
       Serial.println("Send Message");
+      lastSentArray[eventNum] = ms;
+      nextSentArray[eventNum] = ms + fifteenMinutes;
    }
-   else if (counter == 5)
-    counter = 0;
-
-  char str[32]; sprintf(str, "counter = %d", counter);
-   Serial.println(str);
 }
 
 void chooseMessage(int eventNum) {
   Serial.println("choose message function");
-  if (eventNum <= 6)
-    sprintf(messageText, "WARNING: ");
   switch(eventNum){
     case badTemp:
       sprintf(messageText, messages[0]);
@@ -55,10 +59,10 @@ void chooseMessage(int eventNum) {
       sprintf(messageText, messages[1], getBatteryPercentage());
       Serial.println("Bad power set");
       break;
-    case lowBattery:
+   /* case lowBattery:
       sprintf(messageText, messages[3], getBatteryPercentage());
       break;
-   /* case 4:
+      case 4:
       sprintf(messageText, messages[0]);
       break;
     case 5:
@@ -80,3 +84,10 @@ void chooseMessage(int eventNum) {
 
   Serial.print("Message: "); Serial.println(messageText);
 }
+
+ void millisOverflow(int eventNum){
+   unsigned long ms = millis();
+
+   if (ms < lastSentArray[eventNum])
+     nextSentArray[eventNum] -= time_Max;
+  }
