e1b50371bd1dbcf23b13c5a3c7807b9d8e7dbe94
Rylie Breslin <33608974+BreslinRy@users.noreply.github.com>
2021-03-26 09:50:27 -0700

    Update TemperatureDetection.cpp

--- Main/TemperatureDetection.cpp
+++ Main/TemperatureDetection.cpp
@@ -1,8 +1,33 @@
 #include "inc/TemperatureDetection.hpp"
 
+struct constants
+{
+  int ThermistorPin = 0;
+  int Vo;
+  int R0 = 10000;
+  int T0 = 25 + 273;
+  float R1 = 10000;
+  float z, R2, T;
+  float beta = 3950;
+  char UNITS = 'C';
+};
+
 int temperatureChamber;
 bool temperatureInsideBoundries;
 
 bool isTemperatureInsideBoundries() {
   return temperatureInsideBoundries;
+}
+
+void temp_sense(struct constants)
+{
+  struct constants constants;
+  constants.Vo = analogRead(constants.ThermistorPin);
+  constants.R2 = constants.R1 * (1024 / (float)constants.Vo - 1.0);
+  constants.T = (float)1/constants.T0 + (1/constants.beta)*log((float)constants.R2/constants.R0);
+  constants.T = 1/constants.T - 273;
+  constants.T = (constants.T*9/5)+32;
+  Serial.print("Temperature: "); 
+  Serial.print(constants.T);
+  Serial.println("F"); 
 }
