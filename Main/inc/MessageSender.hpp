c38b0e42e0d08fd9e0abb9c907e5ec4b9a3641e9
Rylie Breslin <33608974+BreslinRy@users.noreply.github.com>
2021-03-31 13:45:12 -0700

    Update MessageSender.hpp

--- Main/inc/MessageSender.hpp
+++ Main/inc/MessageSender.hpp
@@ -11,12 +11,10 @@
 //extern char messageText[128];
 //extern char sendTo[8][16]; // How to send to two numbers => Loop that uses a different char every time?
 //extern unsigned long timestamp;
-extern const char *messages[];
+//extern const char *messages[];
 
 void sendText(int eventNum);
 
-char* chooseMessage(int eventNum);
-
-//void millisOverflow(int eventNum);
+//char* chooseMessage(int eventNum);
 
 #endif // _MESSAGE_SENDER_HPP_
